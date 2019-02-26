d27bf0297194920afce5bd31238748dbf2e4a8e5
Philip Puente <philippuente@hotmail.com>
2019-02-26 13:16:51 +0100

    disable debug mode

--- sec/sec/settings.py
+++ sec/sec/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$n%^#g%qx#82w6t^dvjqwv)q*1cy+fwh1ohku7-rbjqcei2^jr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
