23982cb0a3df5713c02418a301193633edb28f90
simenbkr <simen@haitech.org>
2019-02-26 12:47:48 +0100

    Update requirements to require updated version of django.

--- sec/user/dep_passwords.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.hashers import MD5PasswordHasher
-
-# this file is deprecated. Using argon2 instead
-class CustomMD5PasswordHasher(MD5PasswordHasher):
-
-    def salt(self):
-        return "tdt4237"
