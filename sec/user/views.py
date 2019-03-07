8fc3d34a8e54333e255411f67b7b76a304c5ec3b
simenbkr <simen@haitech.org>
2019-03-07 13:15:53 +0100

    Fix form_valid in views.

--- sec/user/views.py
+++ sec/user/views.py
@@ -34,11 +34,12 @@ class LoginView(FormView):
             if user is not None:
                 login(self.request, user)
                 return super().form_valid(form)
-        except IndexError:
-            ip = request.META.get('REMOTE_ADDR')
-            logger.warning('invalid log-in attempt for user: {} from {}'.format(user, ip))
+        except e:
+            ip = self.request.META.get('REMOTE_ADDR')
+            logger.warning('invalid log-in attempt for user: {} from {}'.format(form.cleaned_data['username'], ip))
             form.add_error(None, "Provide a valid username and/or password")  
-            return super().form_invalid(form)
+        return super().form_invalid(form)
+
 
 
 class SignupView(CreateView):
