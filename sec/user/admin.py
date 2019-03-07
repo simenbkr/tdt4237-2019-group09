4038ccedbb98b3cda802593b97921cb77039c7a2
simenbkr <simen@haitech.org>
2019-03-07 17:33:12 +0100

    Add thingies to adminthingy for debugging.

--- sec/user/admin.py
+++ sec/user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, SecurityQuestion, SecurityQuestionUser
 
 
 class ProfileInline(admin.StackedInline):
@@ -32,3 +32,6 @@ class CustomUserAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, SecurityQuestion)
+admin.site.register(User, SecurityQuestionUser)
+admin.site.register(User, Profile)
