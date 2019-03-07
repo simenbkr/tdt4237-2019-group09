7e2f5f90014d6fa82d973ccc92e664ecf92ca0ea
simenbkr <simen@haitech.org>
2019-03-07 17:16:56 +0100

    omegalul

--- sec/user/forms.py
+++ sec/user/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from projects.models import ProjectCategory
-from models import SecurityQuestion
+from .models import SecurityQuestion
 
 
 class SignUpForm(UserCreationForm):
