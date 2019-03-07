6a7e1b18f057556683d774f0ffec5890c660b36c
simenbkr <simen@haitech.org>
2019-03-07 15:15:37 +0100

    Fix user urls.

--- sec/user/urls.py
+++ sec/user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
-    path(r'^verify/(?P<token>[0-9a-f]+)/(?P<username>*)$/', views.VerifyUser.as_view(), name='verify')
+    path(r'^verify/(?P<token>[0-9a-f]+)/(?P<username>[\w.@+-]+)/$', views.VerifyUser.as_view(), name='verify'),
+    path('forgot/, ')
 ]
