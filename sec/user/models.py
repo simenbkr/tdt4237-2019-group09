3a7685135780409312950de7445fae950e349ebf
simenbkr <simen@haitech.org>
2019-03-07 17:29:48 +0100

    Add tostr method for sec quest.

--- sec/user/models.py
+++ sec/user/models.py
@@ -47,6 +47,9 @@ class SecurityQuestion(models.Model):
     id = models.AutoField(primary_key=True)
     question = models.CharField(max_length=200, null=False)
 
+    def __str__(self):
+        return self.question
+
 
 class SecurityQuestionUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
