a814b0a1012a8220b00325aacaae625497ebf12b
simenbkr <simen@haitech.org>
2019-03-12 10:32:25 +0100

    Further improvements to the configuration of the session middleware.

--- sec/sec/middleware.py
+++ sec/sec/middleware.py
@@ -1,6 +1,7 @@
 import django
 import sys
 import platform
+from time import time
 
 from django.conf import settings
 from django.contrib.sessions.backends.base import UpdateError
@@ -8,6 +9,7 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.exceptions import SuspiciousOperation
 from django.utils.cache import patch_vary_headers
 from django.contrib.sites.models import Site
+from django.utils.http import http_date
 
 
 class InformationMiddleware:
@@ -21,6 +23,10 @@ class InformationMiddleware:
 
 
 class SimpleSessionMiddleware(SessionMiddleware):
+
+    def process_request(self, request):
+        session_token = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
+        request.session = self.SessionStore(session_token)
 
     def process_response(self, request, response):
         domain = Site.objects.get_current().domain.split(':')[0]
@@ -39,7 +45,15 @@ class SimpleSessionMiddleware(SessionMiddleware):
                 )
             if accessed:
                 patch_vary_headers(response, ("Cookie",))
-            if modified and not empty:
+            if (modified or settings.SESSION_SAVE_EVERY_REQUEST) and not empty and response.status_code != 500:
+
+                if settings.SESSION_EXPIRE_AT_BROWSER_CLOSE:
+                    max_age = None
+                    expires = None
+                else:
+                    max_age = request.session.get_expiry_age()
+                    expires = http_date(time.time() + max_age)
+
                 try:
                     request.session.save()
                 except UpdateError:
@@ -50,7 +64,7 @@ class SimpleSessionMiddleware(SessionMiddleware):
                     )
                 response.set_cookie(
                     settings.SESSION_COOKIE_NAME,
-                    request.session.session_key, max_age=settings.SESSION_COOKIE_AGE,
+                    request.session.session_key, max_age=max_age, expires=expires,
                     domain=domain,
                     path=settings.SESSION_COOKIE_PATH,
                     secure=settings.SESSION_COOKIE_SECURE,
