69f810bed76e70b4bb566ac76965e00df950b54d
simenbkr <simen@haitech.org>
2019-03-13 13:11:07 +0100

    satan

--- sec/sec/urls.py
+++ sec/sec/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('projects/', include('projects.urls')),
     path('payment/', include('payment.urls')),
