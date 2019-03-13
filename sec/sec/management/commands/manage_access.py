88af4bd9ff517242db5888b9c97569984fd649f7
simenbkr <simen@haitech.org>
2019-03-13 13:56:26 +0100

    woopscoopdepoop

---
+++ sec/sec/management/commands/manage_access.py
@@ -0,0 +1,34 @@
+from user.models import AccessAttempt
+from django.core.management.base import BaseCommand
+
+
+class AccessCommand(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        if options['reset']:
+            objects = AccessAttempt.objects.all()
+            num = len(list(objects))
+            objects.delete()
+            print("Deleted {} records.".format(num))
+
+        elif options['list']:
+            objects = AccessAttempt.objects.all()
+            out = ''
+            for object in objects:
+                out += "{} | {} | {} | {} | {}".format(object.ip_addr, object.username, object.attempt_time,
+                                                       object.user_agent, object.login_valid)
+
+            print(out)
+
+    def add_arguments(self, parser):
+
+        parser.add_argument(
+            '--reset',
+            help='Reset the login attempts for all IPs'
+        )
+
+        parser.add_argument(
+            '--list',
+            help='List login attempts.'
+        )
