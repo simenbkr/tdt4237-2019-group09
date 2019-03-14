d9988853dc5196c9aedcbdd0d99bede8e7e24fad
xADudex <kristianint@outlook.com>
2019-03-14 19:10:28 +0100

    aawf

--- sec/projects/forms.py
+++ sec/projects/forms.py
@@ -88,12 +88,11 @@ class TeamForm(forms.ModelForm):
 
 
 class TeamAddForm(forms.ModelForm):
-    #members = forms.ModelMultipleChoiceField(queryset=Profile.objects.all().filter(pk__in=[a.user.pk for a in task.project.participants.all()]), label='Members with read')
 
     def __init__(self, task, *args, **kwargs):
         super(TeamAddForm, self).__init__()
         user = User.objects.filter(pk__in=[a.user.pk for a in task.project.participants.all()])
-        self.fields['user'] = forms.ModelChoiceField(queryset=user)
+        self.fields['members'] = forms.ModelMultipleChoiceField(queryset=user)
 
     class Meta:
         model = Team
