e400d5e8777262ef2cc1d452711c8bca4f47ee93
simenbkr <simen@haitech.org>
2019-03-14 13:10:19 +0100

    :)

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # /srv/www-data/group09/
 # If we are not, the configuration files for the nginx and group09 services must be edited.
 
-apt update && apt install nginx python3 python3-pip
+apt update && apt install nginx python3 python3-pip -y
 
 systemctl stop nginx
 
@@ -13,11 +13,17 @@ virtualenv -p python3 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
+python sec/manage.py makemigrations
 python sec/manage.py migrate
 python sec/manage.py loaddata init.json
 
-ln -s $PWD/group09.service /etc/systemd/system/group09.service
-ln -s $PWD/nginx-configuration-file /etc/nginx/sites-enabled/default
+python sec/manage.py collectstatic
+
+ln -fs $PWD/group09.service /etc/systemd/system/group09.service
+ln -fs $PWD/nginx-configuration-file /etc/nginx/sites-enabled/default
+
+chown -R www-data:www-data .
+
 
 systemctl start group09
 systemctl start nginx
