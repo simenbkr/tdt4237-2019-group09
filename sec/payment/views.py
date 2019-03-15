f9819e63ff42108ca8b72871dbb5fffcf27bbaa1
simenbkr <simen@haitech.org>
2019-03-15 11:34:49 +0100

    Only team members and the project giver is able to view the receipt.

--- sec/payment/views.py
+++ sec/payment/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, CreateView, FormView
 
-from projects.models import Project, Task
+from projects.models import Project, Task, Team
 from projects.templatetags.project_extras import get_accepted_task_offer
 from .forms import PaymentForm
 from .models import Payment
+from django.http import Http404
 
 
 def payment(request, project_id, task_id):
@@ -27,6 +28,12 @@ class ReceiptView(TemplateView):
 
     def get_context_data(self, project_id, task_id, **kwargs):
         context_data = super().get_context_data(**kwargs)
+
+        team_members = Team.objects.get(task=task_id).members
+        if self.request.user not in list(team_members) and \
+                self.request.user is not Project.objects.get(pk=project_id).user:
+            raise Http404
+
         task = Task.objects.get(pk=task_id)
         context_data.update({
             "project": Project.objects.get(pk=project_id),
